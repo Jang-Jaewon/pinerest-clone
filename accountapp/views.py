4e2ea81dd017fc65b15f82217fd5e2408caa3e2d
Jaewon Jang <jewon119@gmail.com>
2021-09-24 17:52:17 +0900

    Make decorations.py for login_required and preventing others from accessing information.

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,15 +1,25 @@
+from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 from django.contrib.auth.models import User
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from accountapp.decorators import account_ownership_required
 from . import forms
 
+has_ownership = [login_required, account_ownership_required]
 
 # Create your views here.
+@login_required
 def hello_world(request):
-    return render(request, "accountapp/hello_world.html")
+
+    if request.user.is_authenticated:
+        return render(request, "accountapp/hello_world.html")
+    else:
+        return HttpResponseRedirect(reverse("accountapp:login"))
 
 
 # 회원가입 CBV
@@ -40,6 +50,8 @@ class AccountDetailView(DetailView):
 
 
 # 프로필 수정 CBV : AccountCreateView과 거의 유사
+@method_decorator(has_ownership, "get")
+@method_decorator(has_ownership, "post")
 class AccountUpdateView(UpdateView):
     model = User  # 👈 Django의 내장된 User 모델 사용
     context_object_name = "target_user"
@@ -50,6 +62,9 @@ class AccountUpdateView(UpdateView):
     template_name = "accountapp/update.html"  # 👈 수정 양식 Template
 
 
+# @login_required
+@method_decorator(has_ownership, "get")
+@method_decorator(has_ownership, "post")
 class AccountDeleteView(DeleteView):
     model = User  # 👈 Django의 내장된 User 모델 사용
     context_object_name = "target_user"
