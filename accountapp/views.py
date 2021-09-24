765cd4bfbf0152a37a449c143b6f75233bedad73
Jaewon Jang <jewon119@gmail.com>
2021-09-24 11:22:20 +0900

    Create simple login & logout

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,6 +1,19 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView, UpdateView
+from django.contrib.auth.models import User
+from django.urls import reverse, reverse_lazy
+
 
 # Create your views here.
 def hello_world(request):
-    return HttpResponse("Hello world!")
+    return render(request, "accountapp/hello_world.html")
+
+
+class AccountCreateView(CreateView):
+    model = User  # 👈 Django의 내장된 User 모델 사용
+    form_class = UserCreationForm  # 👈 Django의 내장된 Form 사용
+    success_url = reverse_lazy(
+        "accountapp:hello_world"
+    )  # 👈 성공 시 이동할 곳, 반드시 reverse_lazy
+    template_name = "accountapp/create.html"  # 👈 입력 받을 Template
--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -111,8 +111,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+from django.urls import reverse_lazy
+
+LOGIN_REDIRECT_URL = reverse_lazy("accountapp:hello_world")
+LOGOUT_REDIRECT_URL = reverse_lazy("accountapp:login")
